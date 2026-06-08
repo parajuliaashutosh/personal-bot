93c235612fda940647328a132420437923b7559d
Aashutosh Parajuli <aashutosh282@gmail.com>
2026-06-08 14:02:11 +0545

    update limiter

--- app/limiter.py
+++ app/limiter.py
@@ -1,4 +1,20 @@
+from h11 import Request
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
+
+def get_client_ip(request: Request) -> str:
+    # Cloudflare
+    if cf_ip := request.headers.get("CF-Connecting-IP"):
+        return cf_ip
+
+    # Other reverse proxies/load balancers
+    if xff := request.headers.get("X-Forwarded-For"):
+        # First IP is the original client
+        return xff.split(",")[0].strip()
+
+    # Fallback
+    return request.client.host
+
+
+limiter = Limiter(key_func=get_client_ip)
